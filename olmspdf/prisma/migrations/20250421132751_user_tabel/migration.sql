2e47ab3053711b41a4e04c91a7201232bb9d1e56
vineetha <marganavineetha1711@gmail.com>
2025-04-21 19:02:06 +0530

    users tabel creation using prisma

---
+++ olmspdf/prisma/migrations/20250421132751_user_tabel/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "ROLE" AS ENUM ('ADMIN', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "role" "ROLE" NOT NULL DEFAULT 'USER',
+    "forgotPasswordToken" TEXT,
+    "forgotPasswordTokenExpiry" TIMESTAMP(3),
+    "verifyToken" TEXT,
+    "verifyTokenExpiry" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
